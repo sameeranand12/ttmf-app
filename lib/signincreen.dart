17a8a7ff3f2ddece9123e2a3eaa512f05b33cf54
sameeranand12 <201951134@iiitvadodara.ac.in>
2021-08-31 05:26:18 +0530

    Sign up screen done

--- lib/signincreen.dart
+++ lib/signincreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:ttmf/enterfield.dart';
 import 'package:ttmf/homepage.dart';
 
 
@@ -19,27 +20,85 @@ class _SignState extends State<Sign> {
         appBar: AppBar(
           backgroundColor:Color(0xffDD8E31),
 
-        ), //AppBar
-     body: Column(
-       children: [
-         Container(
-           child: InkWell(
-             onTap: () {
-               Navigator.pop(context);
-                          // Navigator.push(
-                          //   context,
-                          //   MaterialPageRoute(
-                          //     builder: (context) => HomePage(),
-                          //   ),
-                          // );
-                        },
+        ),
 
-             child: Icon(
-               Icons.arrow_back_ios_outlined),
+     body: SingleChildScrollView(
+       child: Column(
+ crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+           Container(
+             margin: EdgeInsets.only(top: 20.5 ,left: 20 ),
+             child: InkWell(
+               onTap: () {
+                 Navigator.pop(context);
+                            // Navigator.push(
+                            //   context,
+                            //   MaterialPageRoute(
+                            //     builder: (context) => HomePage(),
+                            //   ),
+                            // );
+                          },
+
+               child: Icon(
+                 Icons.arrow_back,
+               size: 30,),
+             ),
+        ),
+SizedBox(
+  height: 75,
+),
+           Container(
+             margin: EdgeInsets.only( left: 20),
+             child: Text(
+               'Create Account',style: TextStyle(
+               fontWeight: FontWeight.w700,
+               fontStyle: FontStyle.normal,
+               fontSize: 26,
+
+             ),
+             ),
            ),
-      ),
+           SizedBox(
+             height: 35,
+           ),
+           EnterField('Name',  Icons.person,),
+           EnterField('Email',Icons.mail_rounded),
+           EnterField('Password', Icons.lock_rounded ,isPassword: true , ),
+          EnterField('Confirm Password',Icons.lock_rounded,  isPassword: true),
 
-       ],
+           Container(
+
+             height: 40,
+             width: 130,
+             decoration: BoxDecoration(
+                 color: Color(0xffDD8E31),
+                 borderRadius: BorderRadius.circular(47)
+             ),
+             margin: EdgeInsets.only(left: 260 , top: 30),
+             child: Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: [Center(
+                 child: Text(
+                   'SIGN UP',
+                   style: TextStyle(
+                     color: Colors.white,
+                     fontSize: 18,
+                     fontStyle: FontStyle.normal,
+                     fontWeight: FontWeight.w700,
+                   ),
+                 ),
+               ),
+                 SizedBox(width: 6,),
+                 Container(
+                   child: Icon(Icons.arrow_forward_outlined,
+                   color: Colors.white,),
+                 ),
+        ],
+             ),
+           ),
+
+         ],
+       ),
      ),
       ),
 
