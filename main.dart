6f0e7fd5210af17887f4ec58eab325acb33b3739
PulkitDhirana1211 <54793200+PulkitDhirana1211@users.noreply.github.com>
2021-08-31 09:51:19 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:ttmg/SecondPage.dart';
+
+import 'ThirdPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: bnv(),
+    );
+  }
+}
+class bnv extends StatefulWidget {
+  const bnv({Key? key}) : super(key: key);
+
+  @override
+  _bnvState createState() => _bnvState();
+}
+
+class _bnvState extends State<bnv> {
+  int _selectedPage = 0;
+  List<Widget> pageList = [];
+  @override
+  void initState() {
+    pageList.add(HomeScreen());
+    pageList.add(Home2());
+    pageList.add(Notifications());
+    //  pageList.add(LocationScreen());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: IndexedStack(
+        index: _selectedPage,
+        children: pageList,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_location),
+            label: 'Location',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_alert),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded),
+            label: 'Profile',
+          ),
+
+        ],
+        currentIndex: _selectedPage,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedPage = index;
+    });
+  }
+
+}
+
