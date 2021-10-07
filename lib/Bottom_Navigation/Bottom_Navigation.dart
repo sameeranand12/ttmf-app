87efef295004237dffe7a5d1a534414880e2f695
PulkitDhirana1211 <54793200+PulkitDhirana1211@users.noreply.github.com>
2021-10-07 21:42:35 +0530

    Update Bottom_Navigation.dart

--- lib/Bottom_Navigation/Bottom_Navigation.dart
+++ lib/Bottom_Navigation/Bottom_Navigation.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -94,4 +95,55 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+}
+class Mapq extends StatefulWidget {
+  const Mapq({Key? key}) : super(key: key);
+
+  @override
+  _MapqState createState() => _MapqState();
+}
+
+class _MapqState extends State<Mapq> {
+  late GoogleMapController mapController;
+  late Marker origin;
+  Set<Marker> _markers={};
+  final LatLng _center = const LatLng(28.7041, 77.1025);
+  void _onMapCreated(GoogleMapController controller){
+    mapController = controller;
+    setState(() {
+      _markers.add(
+        Marker(markerId: MarkerId('Id 1'), position: LatLng(28.7041, 77.1025)),
+      );
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          markers: Set<Marker>.of(
+            <Marker>[
+              Marker(
+                  onTap: () {
+                    print('Tapped');
+                  },
+                  draggable: true,
+                  markerId: MarkerId('Marker'),
+                  position: _center,
+                  onDragEnd: ((newPosition) {
+                    print(newPosition.latitude);
+                    print(newPosition.longitude);
+                  }))
+            ],
+          ),
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+
 }
