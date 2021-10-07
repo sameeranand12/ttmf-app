b06c6672e1993dd6d46cf32bdd52159609d769c0
PulkitDhirana1211 <54793200+PulkitDhirana1211@users.noreply.github.com>
2021-10-07 21:37:36 +0530

    Create Bottom_Navigation_startpage.dart

---
+++ lib/Bottom_Navigation/Bottom_Navigation_startpage.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:ttmg/Bottom_Navigation.dart';
+
+import 'Bottom_Navigation_Notifications.dart.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: bottomnavigate(),
+    );
+  }
+}
+class bottomnavigate extends StatefulWidget {
+  const bottomnavigate({Key? key}) : super(key: key);
+
+  @override
+  _bottomnavigateState createState() => _bottomnavigateState();
+}
+
+class _bottomnavigateState extends State<bottomnavigate> {
+  int _selectedPage = 0;
+  List<Widget> pageList = [];
+  @override
+  void initState() {
+    pageList.add(HomeScreen());
+    pageList.add(Home2());
+    pageList.add(Notifications());
+    //  pageList.add(LocationScreen());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: IndexedStack(
+        index: _selectedPage,
+        children: pageList,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_location),
+            label: 'Location',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_alert),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded),
+            label: 'Profile',
+          ),
+
+        ],
+        currentIndex: _selectedPage,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedPage = index;
+    });
+  }
+
+}
