17f43f76142e68f1d9c791ecc62c739643e85f33
PulkitDhirana1211 <54793200+PulkitDhirana1211@users.noreply.github.com>
2021-10-28 00:59:09 +0530

    Update Bottom_Navigation_startpage.dart

--- lib/Bottom_Navigation/Bottom_Navigation_startpage.dart
+++ lib/Bottom_Navigation/Bottom_Navigation_startpage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ttmf/profilescreen/screen/my_profile.dart';
-import '../homepage.dart';
 import 'Bottom_Navigation.dart';
 import 'Bottom_Navigation_Notifications.dart';
 
@@ -17,49 +16,49 @@ class _bottomnavigateState extends State<bottomnavigate> {
   List<Widget> pageList = [];
   @override
   void initState() {
-    pageList.add(HomePage());
     pageList.add(WelcomeScreen());
     pageList.add(Mapq());
     pageList.add(Notifications());
-    pageList.add(MyProfilePage());
+    pageList.add(MyProfilePage2());
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _selectedPage,
-        children: pageList,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_outlined),
-            label: 'Appointment',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_location),
-            label: 'Location',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_alert),
-            label: 'Notifications',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle_rounded),
-            label: 'Profile',
-          ),
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xffDD8E31),
+        ),
+        body: IndexedStack(
+          index: _selectedPage,
+          children: pageList,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_location),
+              label: 'Location',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_alert),
+              label: 'Notifications',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle_rounded),
+              label: 'Profile',
+            ),
 
-        ],
-        currentIndex: _selectedPage,
-        selectedItemColor: Colors.blue,
-        onTap: _onItemTapped,
+          ],
+          currentIndex: _selectedPage,
+          selectedItemColor: Colors.blue,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
